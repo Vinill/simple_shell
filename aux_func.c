338af16fb8f45d4ce69a6e615947bd3dcf89d6da
root <martin.liporace@gmail.com>
2022-04-08 03:10:16 -0700

    aux_func.c

---
+++ aux_func.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+* _strlen - returns the length of a string.
+* @s: string
+* Return: length of s
+*/
+
+int _strlen(char *s)
+
+{
+int c = 0;
+while (s[c] != '\0')
+{
+c++;
+}
+return (c);
+}
+
+
+/**
+* _strcmp - compares two strings.
+* @s1: first string
+* @s2: second string
+* Description: show "0" if the two values are equal
+* Return: Always 0 (Success)
+*/
+
+int _strcmp(char *s1, char *s2)
+
+{
+	int cont, val;
+
+	cont = 0;
+	while (s1[cont] == s2[cont] && s1[cont] != '\0')
+	{
+		cont++;
+	}
+
+	val = s1[cont] - s2[cont];
+	return (val);
+}

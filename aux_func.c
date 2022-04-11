f1b0ece4ec60db0e44c2e377aaef345b2bdbf39e
Dak4rPrO <shinoflow111@gmail.com>
2022-04-11 11:55:59 -0700

    todo

--- aux_func.c
+++ aux_func.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * _strlen - returns the length of a string.
@@ -8,16 +7,13 @@
 */
 
 int _strlen(char *s)
-
 {
-int c = 0;
-while (s[c] != '\0')
-{
-c++;
-}
-return (c);
-}
+	int c = 0;
 
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
 
 /**
 * _strcmp - compares two strings.
@@ -28,53 +24,27 @@ return (c);
 */
 
 int _strcmp(char *s1, char *s2)
-
 {
 	int cont, val;
 
 	cont = 0;
 	while (s1[cont] == s2[cont] && s1[cont] != '\0')
-	{
 		cont++;
-	}
 
 	val = s1[cont] - s2[cont];
 	return (val);
 }
 
-
-
-
 /**
 * _getenv - prints the current environment
-*
-*
-*
 * Return: Always 0 (Success)
 */
 
-
-
-
-
-
-
-
 int _getenv(void)
-
 {
-
-
-
 	char **s = environ;
 
 	for (; *s; s++)
-
-	{
-
-	printf("%s\n\n", *s);
-	}
-
-return (0);
+		printf("%s\n\n", *s);
+	return (0);
 }
-
